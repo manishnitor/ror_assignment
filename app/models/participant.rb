d07348623b7d605f2a3f0f264261879af435153a
Manish.Kongari <manishkongari@NIT-MAC-R51.local>
2023-06-08 16:30:48 +0530

    validation

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,5 @@
 class Participant < ApplicationRecord
+  validates :name, :gender, presence: true
   enum :gender, { male: 0, female: 1 }
   has_many :registries , -> { distinct }, through: :enrollments
   has_many :enrollments
