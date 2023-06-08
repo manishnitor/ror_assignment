b95e7722ceccbf9a262d37be5eb8d32a61a956d0
Manish.Kongari <manishkongari@NIT-MAC-R51.local>
2023-06-08 16:29:14 +0530

    first commit

--- app/models/coordinator.rb
+++ app/models/coordinator.rb
@@ -1,5 +1,6 @@
 class Coordinator < ApplicationRecord
   validates :email, presence: true
+  validates :email, uniqueness: true
   has_many :coordinator_registries
   has_many :registries, through: :coordinator_registries
   has_many :enrollments
